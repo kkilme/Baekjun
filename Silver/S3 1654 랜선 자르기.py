ff468a91495f7e612eb445fba881bb58b492418f
kkilme <jojo5142@naver.com>
2021-07-20 19:01:51 +0900

    first time to learn binary search

--- Silver/S3 1654 랜선 자르기.py
+++ Silver/S3 1654 랜선 자르기.py
@@ -6,4 +6,32 @@ arr = []
 for _ in range(k):
     arr.append(int(sys.stdin.readline()))
 
-print(arr)
+high = max(arr)
+low = 1
+
+# 시간초과
+# sum = 0
+
+# for i in range(n, 1, -1):
+#     for j in arr:
+#         sum += j//(m//i)
+    
+#     if sum < n :
+#         if i == 1 : print(m)
+#         else : 
+#            print(m//(i+1))
+#            break
+#     else : sum = 0
+
+# 이분탐색
+while low <= high:
+    mid = (low+high)//2
+    sum = 0
+
+    for i in arr:
+        sum += i//mid
+    
+    if sum < n : high = mid - 1
+    else : low = mid + 1
+
+print(high)
