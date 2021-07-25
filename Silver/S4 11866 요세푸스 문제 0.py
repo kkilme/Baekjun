ac58d1da33c3237ebb25cc62da8d212cdec143f8
kkilme <jojo5142@naver.com>
2021-07-26 03:03:42 +0900

    idk how this works but it did work

--- Silver/S4 11866 요세푸스 문제 0.py
+++ Silver/S4 11866 요세푸스 문제 0.py
@@ -1,9 +1,14 @@
 a, b = map(int, input().split())
 
 l = list(i+1 for i in range(a))
-yose = []
-t = b
-while len(l) != 0 :
-    
+arr = []
 
-print(yose)
+delnum = b-1
+while len(l):
+    if delnum >= len(l):
+        delnum = delnum % len(l)
+    else :
+        arr.append(str(l.pop(delnum)))
+        delnum += b-1
+
+print('<',", ".join(arr),">", sep='')
