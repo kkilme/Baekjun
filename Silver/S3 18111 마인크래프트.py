2b137ac1d44c4bb110538a0a16bbd18bc71a9928
kkilme <jojo5142@naver.com>
2021-07-28 03:59:36 +0900

    passed with python 3

--- Silver/S3 18111 마인크래프트.py
+++ Silver/S3 18111 마인크래프트.py
@@ -2,36 +2,35 @@ import sys
 
 n, m, b = map(int, sys.stdin.readline().split())
 
-
-l = []
-
+l = dict()
+sum = 0
 time = 1000000000000000000000000000000
 
 for _ in range(n):
     k = list(map(int, sys.stdin.readline().split()))
-    for i in k:
-        l.append(i)
+    for s in k:
+        if s in l : l[s]+=1 
+        else : l.update({s:1})
 
-sum = sum(l)
+for j in l.keys():
+    sum += j*l[j]
 
 for i in range(257):
     
+    t = 0
     output = 0
     inp = 0
 
     if sum + b >= i * n * m:
-        for j in l:
-            if i > j :
-                output += i - j
-            elif i < j : 
-                inp += j - i
+        
+        for k in l.keys():
+            if i > k :
+                t += (i - k) * l[k]
+            elif i < k : 
+                t += (k - i) * 2 * l[k]
 
-        if output > inp + b : continue
-        else :
-            t = inp*2 + output
-            
-            if time >= t : 
-                time = t
-                h = i
+        if time >= t : 
+            time = t
+            h = i
 
 print(time, h)
