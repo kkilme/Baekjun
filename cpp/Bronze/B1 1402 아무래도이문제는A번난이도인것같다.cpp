c7f73872e8c6f8d32795a3442c71015da229385f
kkilme <jojo5142@naver.com>
2021-08-25 23:41:23 +0900

    actually huge trap problem

---
+++ cpp/Bronze/B1 1402 아무래도이문제는A번난이도인것같다.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int t, a, b;
+
+    cin >> t;
+
+    for(int i=0;i<t;i++)
+    {
+        cin >> a >> b;
+    }
+
+    for(int i=0;i<t;i++)
+    {
+        cout << "yes" << "\n";
+    }
+
+}
