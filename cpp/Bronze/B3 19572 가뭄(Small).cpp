6e5af66784626e69e32c9bcf47cba6dd2a34bcdc
kkilme <jojo5142@naver.com>
2021-08-25 23:44:04 +0900

    struggled a bit, but it was actually a simple math stuff

---
+++ cpp/Bronze/B3 19572 가뭄(Small).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int d1, d2, d3;
+    float a,b,c;
+    
+    cin >> d1 >> d2 >> d3;
+
+    a = (d1+d2-d3)/2.0;
+    b = d1 - a;
+    c = d2 - a;
+
+    if (a>0 && b>0 && c>0)
+    {
+        cout << "1" << "\n";
+        printf("%.1f %.1f %.1f", a,b,c);
+    }
+    else {cout << "-1";}
+    
+    
+
+}
