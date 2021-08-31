2c8e764307749984f2a142942b6d1517c3433995
kkilme <jojo5142@naver.com>
2021-08-31 23:30:10 +0900

    easy stuffs

---
+++ cpp/Bronze/B5 20492 세금.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main(){
+  
+    int n;
+    cin >> n;
+    cout << fixed << setprecision(0) << n*0.78 << " " << n - (n*0.2*0.22);
+
+}
