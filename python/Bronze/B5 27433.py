b46eaaa1c12c69ed495a64aeddff29da25ee7f41
kkilme <jojo5142@naver.com>
2023-12-25 19:57:56 +0900

    1874, 27433
    for solved.ac christmas bg

---
+++ python/Bronze/B5 27433.py
@@ -0,0 +1,3 @@
+# 팩토리얼 2
+import math
+print(math.factorial(int(input())))
--- python/Silver/S2 1874.py
+++ python/Silver/S2 1874.py
@@ -1,21 +1,28 @@
+# 스택 수열
+import sys
 n = int(input())
 l = []
 for i in range(n):
-    l.append(input())
-printl = []
+    l.append(sys.stdin.readline())
+    
+prints = []
+stack = []
 idx = 0
-cur = 1
-count = 0
-increasing = False
+cur = 0
+
 while idx != len(l):
-    if l[idx] >= cur and not increasing:
-        increasing = True
-        printl.append(['+', l[idx] - cur + 1])
-        printl.append(['-', 1])
-        cur = l[idx]
-        idx+=1
-    elif l[idx] < cur:
-        increasing = False
-
-
-print('end')
+    now = int(l[idx])
+    if now <= cur:
+        if len(stack) != 0 and stack.pop() == now:
+            prints.append('-')
+            idx += 1
+        else:
+            prints = ['NO']
+            break
+    else:
+        while cur != now:
+            cur += 1
+            stack.append(cur)
+            prints.append('+')
+    
+for w in prints: print(w)
