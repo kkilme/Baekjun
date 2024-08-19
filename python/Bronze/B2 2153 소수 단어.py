380d5804dbd81eb138b8f319fd70b35ec95abe8d
kkilme <jojo5142@naver.com>
2024-08-19 10:10:00 +0900

    티어 변경된 문제 파일 이름 수정

--- python/Bronze/B2 2153 소수 단어.py
+++ python/Bronze/B2 2153 소수 단어.py
@@ -1,19 +1,22 @@
+import string
+
 word = input()
 
-alpha = ['null','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
-         'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+alpha = '0' + string.ascii_lowercase + string.ascii_uppercase
 n = 0
 
 for k in range(len(word)):
-    t=0
-    while word[k] != alpha[t] :
-        t+=1
-    else : 
+    t = 0
+    while word[k] != alpha[t]:
+     t += 1
+    else:
      n = n+t
 
-for i in range(2,n) :
-     if n == 1 : print("It is a prime word.")
-     elif n % i == 0 :
+for i in range(2, n):
+    if n == 1:
+         print("It is a prime word.")
+    elif n % i == 0:
          print("It is not a prime word.")
          break
-else : print("It is a prime word.")
+else:
+    print("It is a prime word.")
