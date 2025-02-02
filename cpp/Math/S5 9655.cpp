33ee18a8f430b210d6d09efadf567925c24ab0fa
kkilme <jojo5142@naver.com>
2025-02-03 02:29:33 +0900

    S5 9655 돌 게임

---
+++ cpp/Math/S5 9655.cpp
@@ -0,0 +1,15 @@
+// 2025-02-23 돌 게임
+// 수학 / 게임 이론 / 다이나믹 프로그래밍?
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    if(n%2 == 0) cout << "CY";
+    else cout << "SK";
+
+}
