19f99842b984bed26c0e1731a8a744ca54e67cfc
kkilme <jojo5142@naver.com>
2025-03-19 23:43:59 +0900

    B5 28444 HI-ARC=?

---
+++ cpp/Math/B5 28444.cpp
@@ -0,0 +1,12 @@
+// 2025-03-19 HI-ARC=?
+// 수학 / 사칙연산
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int h,i,a,r,c;
+    cin >> h >> i >> a >> r >> c;
+    cout << h*i - a*r*c;
+}
