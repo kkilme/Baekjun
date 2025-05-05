720a8b4d9a4f90fc26bf959527e54cde6b06d91d
kkilme <jojo5142@naver.com>
2025-05-05 12:02:09 +0900

    G5 15486/S3 14501 퇴사

---
+++ cpp/DP/G5 15486.cpp
@@ -0,0 +1,28 @@
+// 2025-05-05 퇴사 2
+// DP
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int dp[1500001];
+
+int main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    cin >> n;
+
+    int ans = 0;
+    int t, p;
+    for(int i = 1; i<=n; i++){
+        cin >> t >> p;
+        dp[i] = max(dp[i], dp[i-1]);
+        ans = max(ans, dp[i]);
+        if(i+t > n+1) continue;
+        dp[i+t-1] = max(dp[i+t-1], dp[i-1] + p);
+        ans = max(ans, dp[i]);
+    }
+
+    cout << ans << '\n';
+}
