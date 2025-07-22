23b8e44a8e0c38fe779289129df141afe064279e
kkilme <jojo5142@naver.com>
2025-07-23 00:25:49 +0900

    G3 2473 세 용액

--- CSharp/Baekjun/Template.cs
+++ CSharp/Baekjun/Template.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Text;
-using System.IO;
-using System.Linq;
-using System.Collections.Generic;
-
+﻿using System.Text;
 
 class ProblemTemplate
 {
@@ -11,14 +6,12 @@ class ProblemTemplate
     static StreamWriter sw = new StreamWriter(Console.OpenStandardOutput());
     static StringBuilder sb = new StringBuilder();
 
-    public void Solve()
+    static void _Main()
     {
         int T = int.Parse(sr.ReadLine());
         for (int t = 0; t < T; t++)
         {
             string[] line = sr.ReadLine().Split();
-            int a = int.Parse(line[0]);
-            int b = int.Parse(line[1]);
         }
 
         sw.Write(sb.ToString());
---
+++ CSharp/Baekjun/Two Pointers/G3 2473.cs
@@ -0,0 +1,39 @@
+// 2025-07-23 세 용액
+// 정렬 / 투 포인터 / 이분 탐색?
+class P2473
+{
+    static void Main()
+    {
+        var sr = new StreamReader(Console.OpenStandardInput());
+        int n = int.Parse(sr.ReadLine());
+        List<long> l = sr.ReadLine().Split().Select(long.Parse).ToList();
+
+        l.Sort();
+
+        int a1 = 0, a2 = 1, a3 = 2;
+        long curmin = long.MaxValue;
+        for (int i = 0; i <= n - 3; i++)
+        {
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right)
+            {
+                if (Math.Abs(l[i] + l[left] + l[right]) < curmin)
+                {
+                    a1 = i; a2 = left; a3 = right;
+                    curmin = Math.Abs(l[i] + l[left] + l[right]);
+                }
+
+                if (Math.Abs(l[i] + l[left + 1] + l[right]) < Math.Abs(l[i] + l[left] + l[right - 1]))
+                {
+                    left++;
+                }
+                else
+                {
+                    right--;
+                }
+            }
+        }
+        Console.WriteLine($"{l[a1]} {l[a2]} {l[a3]}");
+    }
+}
