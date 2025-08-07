3ad6b8707e417dc045dda90825499c1258a5ee4a
kkilme <jojo5142@naver.com>
2025-08-07 22:24:30 +0900

    G3 13904 과제

--- CSharp/Baekjun/Greedy/G3 13904.cs
+++ CSharp/Baekjun/Greedy/G3 13904.cs
@@ -1,13 +1,14 @@
-// 2025-08-06 순회강연
-// 자료구조 / 그리디 / 정렬 / 우선순위 큐
-class P2109
+// 2025-08-07 과제
+// 자료 구조 / 그리디 / 정렬 / 우선순위 큐
+// 2109와 동일
+class P13904
 {
     static StreamReader sr = new StreamReader(Console.OpenStandardInput());
 
     static void _Main()
     {
         int n = int.Parse(sr.ReadLine());
-        List<(int p, int d)> l = new();
+        List<(int d, int w)> l = new();
         for (int i = 0; i < n; i++)
         {
             var line = sr.ReadLine().Split();
@@ -17,10 +18,11 @@ class P2109
         l.Sort((a, b) => a.d.CompareTo(b.d));
 
         PriorityQueue<int, int> pq = new();
-        foreach (var (p, d) in l)
+
+        foreach (var (d, w) in l)
         {
-            pq.Enqueue(p, p); // 일단 넣고
-            if (d < pq.Count) // 날짜 초과 시 가장 낮은 보수 강연 제거
+            pq.Enqueue(w, w);
+            if (pq.Count > d)
             {
                 pq.Dequeue();
             }
@@ -28,7 +30,7 @@ class P2109
 
         int answer = 0;
 
-        while (pq.Count != 0) answer += pq.Dequeue();
+        while (pq.Count > 0) answer += pq.Dequeue();
 
         Console.Write(answer);
     }
