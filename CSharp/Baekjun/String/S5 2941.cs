9898aadd632fba654661526319a62832b9259656
kkilme <jojo5142@naver.com>
2025-09-11 21:51:11 +0900

    S5 2941 크로아티아 알파벳

---
+++ CSharp/Baekjun/String/S5 2941.cs
@@ -0,0 +1,22 @@
+// 2025-09-11 크로아티아 알파벳
+// 구현 / 문자열
+class P2941
+{
+    static void Main()
+    {
+        var s = Console.ReadLine();
+        int n = s.Length;
+        int ans = 0;
+        for (int i = 0; i < n; i++)
+        {
+            var cur = s[i];
+            if ((cur == 's' || cur == 'z' || cur == 'c') && i + 1 < n && s[i + 1] == '=') i++;
+            else if ((cur == 'n' || cur == 'l') && i + 1 < n && s[i + 1] == 'j') i++;
+            else if (cur == 'd' && i + 2 < n && s[i + 1] == 'z' && s[i + 2] == '=') i += 2;
+            else if ((cur == 'c' || cur == 'd') && i + 1 < n && s[i + 1] == '-') i++;
+            ans++;
+        }
+
+        Console.Write(ans);
+    }
+}
