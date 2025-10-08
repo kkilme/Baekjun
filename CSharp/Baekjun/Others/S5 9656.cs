562c42a9e76112123ff8eeff5dba8e3dd8cff9c7
kkilme <jojo5142@naver.com>
2025-10-08 22:22:34 +0900

    S5 9656 돌 게임 2

---
+++ CSharp/Baekjun/Others/S5 9656.cs
@@ -0,0 +1,20 @@
+// 2025-10-08 돌 게임 2
+// 수학 / DP / 게임 이론
+class P9656
+{
+
+    static void _Main()
+    {
+        Console.Write(int.Parse(Console.ReadLine()) % 2 == 0 ? "SK" : "CY");
+
+        /*
+        1 -> b
+        2 -> a
+        3 -> b
+        4 -> a
+        5 -> b
+        6 -> a
+        */
+
+    }
+}
